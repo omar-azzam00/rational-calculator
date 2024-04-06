44346c8b41b266b8f63f3276da556940db0970fd
Omar Azzam <omar16220061622006@gmail.com>
2024-04-06 14:50:17 +0200

    this tests is not needed anymore. it should be made again professionally.

--- tests/calculatorShell_performMathOperationFrac_unitTest.dart
+++
@@ -1,9 +0,0 @@
-import '../classes/CalculatorShell.dart';
-
-void calculatorShell_performMathOperationFrac_unitTest(CalculatorShell shell) {
-  var tokens = shell.getTokens('120 + 4 * 3 / 4 + 32 / 5');
-
-  if (shell.isValidMathOperation(tokens)) {
-    shell.performMathOperationFrac(tokens);
-  }
-}
